c93d464d41b4e1b3c693d0b96cf3af5ff0f9fe8f
Chinmay-innovates <chinmaygope69@gmail.com>
2024-09-12 00:41:36 +0530

    authentication routes

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //parse JSON request bodies
+app.use(cookieParser());
+app.use("/api/v1/auth", authRoutes);
+
+app.listen(PORT, () => {
+	console.log(`Server listening on ${PORT}`);
+	connectDB();
+});
